f55ba0bfa118a421635e7670c20453cb3064c285
Ga-ok <62277179+Ga-ok@users.noreply.github.com>
2022-09-15 17:44:35 +0900

    Create 이가옥_main.cpp

---
+++ 0915/프로그래머스_모음사전/이가옥_main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+char dir[] = { 'A', 'E', 'I', 'O', 'U' };
+int cnt = 0;
+int result = 0;
+
+void dfs(string str, string word) {
+
+	if (str == word) {
+		result = cnt;
+	}
+
+	if (str.length() >= 5) {
+		return;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		char next = dir[i];
+		cnt += 1;
+		dfs(str + next, word);
+	}
+
+}
+
+int solution(string word) {
+	int answer = 0;
+
+	dfs("", word);
+    
+	answer = result;
+	return answer;
+}
